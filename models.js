54c56944c6561b5b137fe4572b66612a80a35c78
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-10-31 09:33:59 +0100

    style: linting

--- models.js
+++ models.js
@@ -1,45 +1,46 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-    Title:{type: String, required: true},
-    Description:{type:String, required: true},
-    Genres:[String],
-    directors: [String],
-    Actors:[String], 
-    ImagePath: String,
-    Featured: Boolean 
-})
+        Title: { type: String, required: true },
+        Year: Number,
+        Description: { type: String, required: true },
+        Genres: [String],
+        Directors: [String],
+        Actors: [String],
+        ImagePath: String,
+        Featured: Boolean,
+});
 
 const userSchema = new mongoose.Schema({
-    Username: {type:String, required: true},
-    Password:{type:String, required: true},
-    Email: {type:String, required: true},
-    Birthday: Date, 
-    Favslist:[{type:mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
-})
+        Username: { type: String, required: true },
+        Password: { type: String, required: true },
+        Email: { type: String, required: true },
+        Birthday: Date,
+        Favslist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+});
 
 const directorSchema = new mongoose.Schema({
-    Name: {type:String, required:true},
-    YearOfBirth: Number,
-    Bio: String,
-})
+        Name: { type: String, required: true },
+        YearOfBirth: Number,
+        Bio: String,
+});
 
 const genreSchema = new mongoose.Schema({
-    Name: {type:String, required:true},
-    Description: String,
-})
+        Name: { type: String, required: true },
+        Description: String,
+});
 
 const actorSchema = new mongoose.Schema({
-    Name: {type:String, required:true},
-    YearOfBirth: Number,
-    Bio: String,
-})
+        Name: { type: String, required: true },
+        YearOfBirth: Number,
+        Bio: String,
+});
 
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User',userSchema);
-let Director = mongoose.model('Director',directorSchema);
-let Genre = mongoose.model('Genre', genreSchema);
-let Actor = mongoose.model('Actor',actorSchema);
+const Movie = mongoose.model('Movie', movieSchema);
+const User = mongoose.model('User', userSchema);
+const Director = mongoose.model('Director', directorSchema);
+const Genre = mongoose.model('Genre', genreSchema);
+const Actor = mongoose.model('Actor', actorSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;

7b8e6908c6f49fcb240df267d841ef9be09fb323
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-10-31 18:00:06 +0100

    build: add passport package

---
+++ passport.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const Models = require('./models.js');
+
+const Users = Models.User;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(
+        new LocalStrategy(
+                {
+                        usernameField: 'Username',
+                        passwordField: 'Password',
+                },
+                (username, password, callback) => {
+                        console.log(`${username} ${password}`);
+                        Users.findOne({ Username: username }, (error, user) => {
+                                if (error) {
+                                        console.log(error);
+                                        return callback(error);
+                                }
+                                if (!user) {
+                                        console.log('incorrect username');
+                                        return callback(null, false, { message: 'Incorrect username or password.' });
+                                }
+                                console.log('finished');
+                                return callback(null, user);
+                        });
+                }
+        )
+);
+
+passport.use(
+        new JWTStrategy(
+                {
+                        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+                        secretOrKey: 'your_jwt_secret',
+                },
+                (jwtPayload, callback) =>
+                        Users.findById(jwtPayload._id)
+                                .then((user) => callback(null, user))
+                                .catch((error) => callback(error))
+        )
+);

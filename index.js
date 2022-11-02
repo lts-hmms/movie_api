6ab5fddc346311cc5720818f24c335ee4e5758cd
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-02 13:06:13 +0100

    bug: mongoose.connect

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ main().catch((err) => console.log(err));
 async function main() {
         await mongoose.connect('mongodb://localhost:27017/myMoviesDB');
 }
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
 const { check, validationResult } = require('express-validator');

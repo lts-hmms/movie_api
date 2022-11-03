40892fcebf34d7ad99138f4e31a9338fab4440ed
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-11-03 19:39:49 +0100

    feat: populate reference

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
 // get all movies
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
         Movies.find()
-                .populate('Genres')
+                .populate('Genre')
                 .then((movies) => {
                         res.status(200).json(movies);
                 })
